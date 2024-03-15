3d712cc0662ea36134e1e952d5a5d087cfb49a14
Khoi Nguyen <khoipnguyen04@gmail.com>
2024-03-15 17:26:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ButtonGradient />
     </>
   );
 };
